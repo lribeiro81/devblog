9fdb13f337673788c6cda9e880e174d5bdd5423d
Luis Ribeiro <luis.barreto@gmail.com>
2020-03-19 17:40:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'My Gatsby Netflify blog', // Required
+  author: 'Luis Ribeiro', // Required
+  description: 'Solutions Architect',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://student.dei.uc.pt/~lribeiro/',
+    github: 'https://github.com/lribeiro81',
+    twitter: 'https://twitter.com/the_intern_says',
+    linkedin: 'https://www.linkedin.com/in/lfribeiro/',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '',
+  siteUrl: 'https://agitated-goldwasser-987115.netlify.com/',
 };
